aa0de1f0c6428d1fff478e3100d13395bf02d5bc
ZieksQ <nexisonytb@gmail.com>
2026-06-29 03:10:57 +0800

    added TestContainer.Redis

--- tests/Api.IntegrationTests/Infrastructure/CustomWebApplicationFactory.cs
+++ tests/Api.IntegrationTests/Infrastructure/CustomWebApplicationFactory.cs
@@ -2,17 +2,22 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
 using Suppfly.Api.Infrastructure.Persistence;
 
 namespace Api.IntegrationTests.Infrastructure;
 
 public class CustomWebApplicationFactory : WebApplicationFactory<Program>
 {
-  private readonly string _connectionString;
+  private readonly string _pgsqlConnectionString;
+  private readonly string _redisConnectionString;
 
-  public CustomWebApplicationFactory(string connectionString)
+  public CustomWebApplicationFactory(
+      string pgsqlConnectionString,
+      string redisConnectionString)
   {
-    _connectionString = connectionString;
+    _pgsqlConnectionString = pgsqlConnectionString;
+    _redisConnectionString = redisConnectionString;
   }
 
   protected override void ConfigureWebHost(IWebHostBuilder builder)
@@ -30,13 +35,22 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
 
       services.AddDbContext<AppDbContext>(options =>
       {
-        options.UseNpgsql(_connectionString);
+        options.UseNpgsql(_pgsqlConnectionString);
       });
+
+      // Redis desciptor
+      var redisDescriptor = services
+      .SingleOrDefault(d => d.ServiceType == typeof(IConnectionMultiplexer));
+
+      if (redisDescriptor is not null)
+        services.Remove(redisDescriptor);
+
+      services.AddSingleton<IConnectionMultiplexer>(_ =>
+          ConnectionMultiplexer.Connect(_redisConnectionString));
 
       var sp = services.BuildServiceProvider();
 
       using var scope = sp.CreateScope();
-
       var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
 
       db.Database.Migrate();
--- tests/Api.IntegrationTests/Shared/IntegrationTestFixture.cs
+++ tests/Api.IntegrationTests/Shared/IntegrationTestFixture.cs
@@ -9,7 +9,7 @@ namespace Api.IntegrationTests.Shared;
 public class IntegrationTestFixture : IAsyncLifetime
 {
   private readonly PostgreSqlContainer _postgres = new();
-
+  private readonly RedisContainer _redis = new();
 
   public CustomWebApplicationFactory Factory { get; private set; } = default!;
 
@@ -27,13 +27,17 @@ public class IntegrationTestFixture : IAsyncLifetime
     Factory.Dispose();
 
     await _postgres.DisposeAsync();
+    await _redis.DisposeAsync();
   }
 
   public async Task InitializeAsync()
   {
     await _postgres.InitializeAsync();
+    await _redis.InitializeAsync();
 
-    Factory = new CustomWebApplicationFactory(_postgres.ConnectionString());
+    Factory = new CustomWebApplicationFactory(
+        _postgres.ConnectionString(),
+        _redis.ConnectionString());
 
     using var scope = Factory.Services.CreateScope();
 
